787b66e9adcac67e1f8c94974247a4edb0909b91
fasyaais <dyqdvo@gmail.com>
2025-10-22 20:05:06 +0700

    feat: add logger using morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import authMiddleware from './middlewares/authMiddleware.js';
-
 import authRouter from "./routes/authRoute.js"
+import morgan from "morgan";
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan('dev'));
 app.use("/api/v1",authRouter);
 app.get('/test',authMiddleware,(req,res)=> {
-    res.send('Hello');
+    console.log(req.userId);
+    return res.json({
+        message:'success'
+    })
 });
 
 export default app;
