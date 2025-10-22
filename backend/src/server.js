cb78364a6b3b60fdcf28c4ddb77e2fd6a86f8c1a
fasyaais <dyqdvo@gmail.com>
2025-10-22 22:26:42 +0700

    feat: implement Socket.IO for real-time communication and update configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,20 @@ import config from "./config/index.js";
 import app from './app.js';
 import db from './models/index.js';
 import { connectDB } from './config/database.js';
+import { createServer } from "node:http";
+import SocketService from './services/SocketService.js';
+
+const server = createServer(app);
 
 const startServer = async () =>{
     await connectDB()
     await db.sync();
+    const socketService = new SocketService(server);
+    socketService.init();
+    const io = socketService.getIO();
 
-    app.listen(config.APP_PORT,()=>{
-        console.log(`Server berjalan pada http://${config.APP_HOST+":"+config.APP_PORT}`);
+    server.listen(config.APP_PORT,()=>{
+        console.log(`Server running on http://${config.APP_HOST+":"+config.APP_PORT}`);
     });
 }
 
---
+++ backend/src/services/SocketService.js
@@ -0,0 +1,68 @@
+import { Server } from "socket.io";
+import config from "../config/index.js";
+import db from "../models/index.js";
+import { verifyToken } from "../utils/jwtUtil.js";
+
+class SocketService {
+    constructor(httpServer){
+        this.io = new Server(httpServer,{
+            cors : {
+                origin: config.FRONTEND_URL,
+                methods: ["GET","POST"]
+            },
+        });
+        this.userSockets = new Map();
+    }
+
+    init(){
+        this.__authMiddleware();
+        this.__connectionHandler();
+    }
+
+    __authMiddleware(){
+        this.io.use(async (socket,next) => {
+            try {
+                const token = socket.handshake.headers.authorization;
+                const decode = verifyToken(token);
+                const userId = decode.id
+                const user = await db.User.findByPk(userId);
+                if(user == null){
+                    throw new Error("Authentication error");
+                }
+                socket.userId = userId;
+                return next();
+            } catch (error) {
+                next(error);
+            }
+        });
+    }
+
+    __connectionHandler(){
+        this.io.on("connection", async (socket) => {
+            console.log(`User connected: ${socket.userId}`);
+            if(!this.userSockets.has(socket.userId)){
+                this.userSockets.set(socket.userId,new Set());
+            }
+            this.userSockets.get(socket.userId).add(socket.id);
+            socket.on("disconnect",() => {
+                this.__disconnectUserSockets(socket,socket.userId);
+            });
+        });
+    }
+
+    __disconnectUserSockets(socket, userId){
+        if(this.userSockets.has(userId)){
+            this.userSockets.get(userId).delete(socket.id);
+            if(!this.userSockets.get(userId).size === 0){
+                this.userSockets.delete(userId);
+            }
+        }
+        console.log(`Socket ${socket.id} disconnected. User ${userId} have a total of ${this.userSockets.get(userId)?.size || 0} connections left.`);
+
+    }
+    getIO(){
+        return this.io;
+    }
+}
+
+export default SocketService;
