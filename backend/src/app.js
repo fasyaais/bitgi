3d58d7289b9a4fa28d537b9b4eee1f903b31b533
fasyaais <dyqdvo@gmail.com>
2025-11-13 18:35:39 +0700

    feat: implement device management features, including device registration and QR code generation

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import authMiddleware from './middlewares/authMiddleware.js';
 import authRouter from "./routes/authRoute.js"
+import deviceRoute from "./routes/deviceRoute.js"
 import morgan from "morgan";
+import path from 'node:path';
+import {fileURLToPath} from "url"
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(__filename);
+const __dirname = path.join(currentDir,"..");
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use("/api/v1",authRouter);
+app.use('/api/v1/devices',deviceRoute);
 app.get('/test',authMiddleware,(req,res)=> {
     console.log(req.userId);
     return res.json({
