7e3ef96703b147ecbd632fd04a1c3ddc8280078a
muhilalr <muhammadhilalra@gmail.com>
2025-11-17 18:42:37 +0700

    admin get, show, update, delete device

--- backend/src/models/Device.js
+++ backend/src/models/Device.js
@@ -1,44 +1,46 @@
 import { DataTypes } from "sequelize";
 
 export default (sequelize) => {
-    const device = sequelize.define('Device',
-        {
-            id: {
-                type: DataTypes.STRING,
-                primaryKey: true,
-            },
-            user_id : {
-                type: DataTypes.INTEGER,
-                allowNull: true,
-            },
-            name : {
-                type: DataTypes.STRING,
-                allowNull: true,
-            },
-            token : {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            type : {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            is_online : {
-                type: DataTypes.BOOLEAN,
-                defaultValue:false,
-                allowNull: true,
-            },
-            last_seen : {
-                type: DataTypes.TIME,
-                allowNull: true,
-            },
-            qr_code : {
-                type: DataTypes.STRING,
-                allowNull: true,
-            }
-        }, {
-            tableName:'devices',
-        }
-    );
-    return device;
-}
+  const device = sequelize.define(
+    "Device",
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      is_online: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+      },
+      last_seen: {
+        type: DataTypes.TIME,
+        allowNull: true,
+      },
+      qr_code: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "devices",
+    }
+  );
+  return device;
+};
